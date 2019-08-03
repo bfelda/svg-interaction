ba5f994959abfa6738c01cbdb4cab5755a229398
bfelda <bfelda@gmail.com>
2019-08-03 16:58:13 -0500

    tons

--- suprise/starAnime.js
+++ suprise/starAnime.js
@@ -1,12 +1,8 @@
 var dollarPassedIn,
-    shuffleText,
-    activeSponsorAmt,
-    activeUserAmt;
+    shuffleText;
 
-var sponsorAmtElem = $('#sponsor-payout');
-var userAmtElem = $('#user-payout');
-var nums = $('.numberContainer>span');
-var trigger = $('.trigger');
+var nums = document.querySelectorAll('.numberContainer>span');
+var trigger = document.querySelector('.trigger');
 
 var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var shuffleDebounce = 100;  //interval for spinning numbers.  Too fast causes browser lag
@@ -66,51 +62,23 @@ var activateShuffle = function (dollarValue) {
     }
 };
 
-function TextShuffle() {
-    var _this = this;
+function textShuffle() {
+    var dollarsTl = new TimelineLite({ delay: .5 });
+    var starTl = new TimelineLite({ delay: 6.3 });
+    var giftsTl = new TimelineLite({ delay: 6.5 });
 
-    _this.to = function (dollars) {
-        TweenLite.killTweensOf(_this);
-        var offerResult = new TimelineLite();
-        var businessOutro = new TimelineLite({ delay: 4 });
-        var dollarsTl = new TimelineLite({ delay: 4.5 });
-        var starTl = new TimelineLite({ delay: 10.3 });
-        var giftsTl = new TimelineLite({ delay: 11 });
+    //move the dollars down, this is timed with the stripping zero's function
+    dollarsTl.to('.dollars', .2, { 'opacity': 1 })
+        .to('.dollars', 1, { 'top': '+=120px', ease: Elastic.easeOut.config(1, 0.3) }, 5.8);
 
-        // Remove the offer info
-        offerResult.to('.offerResultContainer', .2, { 'opacity': 0 })
-            .set('.offerResultContainer', { 'display': 'none' })
-            .set('.affiliateContainer', { 'display': 'flex' })
-            .to('.affiliateContainer', .2, { 'opacity': 1 });
-
-        // Remove the affiliate logo
-        businessOutro.to('.affiliateContainer', .2, { 'opacity': 0 })
-            .set('.affiliateContainer', { 'display': 'none' });
-
-        //move the dollars down, this is timed with the stripping zero's function
-        dollarsTl.to('.dollars', .2, { 'opacity': 1 })
-            .to('.dollars', 1, { 'top': '+=80px', ease: Elastic.easeOut.config(1, 0.3) }, 5.8);
-
-        //Show the stars and kick off the burst
-        //This has two different animations dependent on the dollar value.  One penny only goes to the organization
-        starTl.to('#stars', .8, { 'opacity': 1, 'scale': 1, ease: Bounce.easeOut })
-            .to('#burst', 1, { 'opacity': 1 })
-            .to('#burst', 100, { rotation: 360, repeat: -1, transformOrigin: "50% 50%", ease: Linear.easeNone });
-        if (dollars > .01) {
-            giftsTl.to('#sponsor-gift', .8, { 'scale': 1, 'opacity': 1, ease: Expo.easeInOut })
-                .to('#sponsor-gift', .8, { 'scale': .6, 'opacity': 1, ease: Expo.easeInOut, x: -100, y: -50 }, 1)
-                // .to('#sponsor-payout', .8, { 'color': 'white' }, '-=.5')
-                .to('#user-boost', .8, { 'scale': 1, 'opacity': 1, ease: Expo.easeInOut })
-                .to('#user-boost', .8, { 'scale': .6, 'opacity': 1, ease: Expo.easeInOut, x: 100, y: -50 })
-                // .to('#user-payout', .8, { 'color': 'white' }, '-=.5')
-                .to('#moving-on', .8, { 'opacity': 1, y: -50, ease: Expo.easeInOut });
-        } else {
-            giftsTl.to('#sponsor-gift', .8, { 'scale': 1, 'opacity': 1, ease: Expo.easeInOut })
-                .to('#sponsor-gift', .8, { 'scale': .5, 'opacity': 1, ease: Expo.easeInOut, y: -50 }, 1)
-                // .to('#sponsor-payout', .8, { 'color': 'white' }, '-=.5');
-                .to('#moving-on', .8, { 'opacity': 1, y: -100, ease: Expo.easeInOut });
-        }
-    };
+    //Show the stars and kick off the burst
+    //This has two different animations dependent on the dollar value.  One penny only goes to the organization
+    starTl.to('#stars', .8, { 'opacity': 1, 'scale': 1, ease: Bounce.easeOut })
+        .to('#burst', 1, { 'opacity': .5 })
+        .to('#burst', 100, { rotation: 360, repeat: -1, transformOrigin: "50% 50%", ease: Linear.easeNone });
+    giftsTl.to('#reward', .8, { 'scale': 1, 'opacity': 1, ease: Expo.easeInOut })
+        .to('#reward', .8, { 'scale': .75, 'opacity': 1, ease: Expo.easeInOut, y: -50 }, 1)   
+        .to('#moving-on', .8, { 'opacity': 1, y: -100, ease: Expo.easeInOut });
 }
 
 var bonusPresentationInit = function () {
@@ -120,27 +88,15 @@ var bonusPresentationInit = function () {
     boostAnime.to('.kickoffBtn', 1.5, { 'opacity': 1, 'y': 0, ease: Expo.easeOut })
         .to('.coinContainer', .8, { 'opacity': 1, ease: Expo.Linear }, '-=.5');
     boostPulse.to('.coinContainer', .6, { 'scale': 1.1 });
-    delayNote.to('#lateNote', .8, { 'opacity': 1 });
 };
-
-var setDistributions = function (orgDollars, userDollars) {
-    activeUserAmt = userDollars.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-    activeSponsorAmt = orgDollars.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-    userAmtElem.html("+ $" + activeUserAmt.toString());
-    sponsorAmtElem.html("+ $" + activeSponsorAmt.toString());
-};
-
-_shuffle = new TextShuffle();
 
 bonusPresentationInit();
 
-var init = function (dollarPassedIn, orgDollars, userDollars) {
+var init = function (dollarPassedIn) {
     shuffleText = transformIntToShuffleString(dollarPassedIn);
-    setDistributions(orgDollars, userDollars);
+    activateShuffle(shuffleText);
 
-    setTimeout(function () {
-        activateShuffle(shuffleText);
-    }, 4000);
-
-    _shuffle.to(dollarPassedIn.toString());
+    textShuffle();
 };
+let value = Math.floor(Math.random() * 100) + 1;
+init(value, value, value);
